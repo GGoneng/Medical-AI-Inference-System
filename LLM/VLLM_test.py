dbec7e400444f1d2883637d0bb4ff60650fb566a
GGoneng <zxzx0031@naver.com>
2026-02-19 04:09:54 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   LLM/VLLM_test.py

---
+++ LLM/VLLM_test.py
@@ -0,0 +1,55 @@
+from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+import os
+
+_BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+_MODEL_NAME = os.path.join(_BASE_PATH, "hari-q3-8b-awq")
+
+def main():
+    model = LLM(
+    _MODEL_NAME,
+    gpu_memory_utilization=0.86,
+    max_model_len=1024,
+    max_num_seqs=1,
+    max_num_batched_tokens=1024,
+    enforce_eager=True
+)
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        _MODEL_NAME,
+        fix_mistral_regex=True
+    )
+
+    sampling_params = SamplingParams(
+        max_tokens=4096
+    )
+
+    prompt = '''
+    ### Instruction:
+    당신은 임상 지식을 갖춘 유능하고 신뢰할 수 있는 한국어 기반 의료 어시스턴트입니다.
+    사용자의 질문에 대해 정확하고 신중한 임상 추론을 바탕으로 진단 가능성을 제시해 주세요.
+    반드시 환자의 연령, 증상, 검사 결과, 통증 부위 등 모든 단서를 종합적으로 고려하여 추론 과정과 진단명을 제시해야 합니다.
+    의학적으로 정확한 용어를 사용하되, 필요하다면 일반인이 이해하기 쉬운 용어도 병행해 설명해 주세요.
+
+    ### Question:
+    60세 남성이 복통과 발열을 호소하며 내원하였습니다.
+    혈액 검사 결과 백혈구 수치가 상승했고, 우측 하복부 압통이 확인되었습니다.
+    가장 가능성이 높은 진단명은 무엇인가요?
+    '''.strip()
+
+    messages = [
+        {"role": "user", "content": prompt}
+    ]
+
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    outputs = model.generate([text], sampling_params)
+    for output in outputs:
+        print(output.outputs[0].text)
+
+if __name__ == "__main__":
+    main()
