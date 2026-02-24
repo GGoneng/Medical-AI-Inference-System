82c7a08351deda9f7a83123e425292d05bbcd66a
GGoneng <zxzx0031@naver.com>
2026-02-25 04:04:55 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   backend/Modules/LLMModules.py
    	modified:   backend/main.py
    	modified:   backend/requirements.txt
    	modified:   docker-compose.yml

--- backend/Modules/LLMModules.py
+++ backend/Modules/LLMModules.py
@@ -6,6 +6,10 @@ from Modules.TypeVariable import *
 
 from langchain_core.prompts.prompt import PromptTemplate
 from langchain_community.llms.vllm import VLLM
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnableLambda
+
+from transformers import AutoTokenizer
 
 import redis
 import pickle
@@ -15,54 +19,30 @@ import asyncio
 # ----------------------------------------------------------
 # Internal Variables (do not call externally)
 # ----------------------------------------------------------
-
-_BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-_MODEL_NAME = "snuh/hari-q3-14b"
-
-_SEGMENTATION_MODELS_PATH = os.path.join(_BASE_PATH, "Models", _MODEL_NAME)
-
-_llm = None
-
-_prompts = {
-"xray" : PromptTemplate(
-input_variables=["symptom"],
-template="""
-    ### Instruction:
-    당신은 임상 지식을 갖춘 유능하고 신뢰할 수 있는 한국어 기반 의료 어시스턴트입니다.
-    사용자의 질문에 대해 정확하고 신중한 임상 추론을 바탕으로 진단 가능성을 제시해 주세요.
-    반드시 환자의 연령, 증상, 검사 결과, 통증 부위 등 모든 단서를 종합적으로 고려하여 추론 과정과 진단명을 제시해야 합니다.
-    의학적으로 정확한 용어를 사용하되, 필요하다면 일반인이 이해하기 쉬운 용어도 병행해 설명해 주세요.
-
-    ### Question:
-    소아 복부 X-ray에서 {symptom}이 발견되었습니다.
-    {symptom}의 증상, 치료 방법을 설명해주세요.
-
-    ### Output:
-    [<end>]
-    """
-),
-
-"question" : PromptTemplate(
-input_variables=["question"],
-template="""
-    ### Instruction:
-    당신은 임상 지식을 갖춘 유능하고 신뢰할 수 있는 한국어 기반 의료 어시스턴트입니다.
-    사용자의 질문에 대해 정확하고 신중한 임상 추론을 바탕으로 진단 가능성을 제시해 주세요.
-    반드시 환자의 연령, 증상, 검사 결과, 통증 부위 등 모든 단서를 종합적으로 고려하여 추론 과정과 진단명을 제시해야 합니다.
-    의학적으로 정확한 용어를 사용하되, 필요하다면 일반인이 이해하기 쉬운 용어도 병행해 설명해 주세요.
-
-    ### Question:
-    {question}
-
-    ### Output:
-    [<end>]
-    """    
-)}
+_BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # ----------------------------------------------------------
 # External Variables (can be called from outside)
 # ----------------------------------------------------------
+
+model_name = "/models/hari-q3-8b-awq"
+tokenizer_name = "Models/hari-q3-8b-awq"
+
+tokenizer_path = os.path.join(_BASE_PATH, tokenizer_name)
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+openai_api_key = "EMPTY"
+openai_api_base = "http://vllm:8000/v1"
+
+max_tokens = 512 
+
+llm = ChatOpenAI(
+    base_url=openai_api_base,
+    api_key=openai_api_key,
+    model=model_name,
+    max_tokens=max_tokens
+)
 
 
 # ----------------------------------------------------------
@@ -74,57 +54,67 @@ template="""
 # External Classes (can be called from outside)
 # ----------------------------------------------------------
 
+class PromptBuilder:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def build(self, messages):
+        prompt_template = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize = False,
+            add_generation_prompt = True,
+            enable_thinking = False
+        )
+
+        return prompt_template
 
 # ----------------------------------------------------------
 # Internal Functions (do not call externally)
 # ----------------------------------------------------------
 
-def _load_model():
-    return VLLM(
-    model=_SEGMENTATION_MODELS_PATH,
-    max_new_tokens=4096,
-    top_k=10,
-    top_p=0.95,
-    temperature=0.8,
-    tensor_parallel_size=1,
-    vllm_kwargs={
-        "gpu_memory_utilization": 0.7,
+def _build_messages(question: str):
+    return [
+        {
+            "role": "system",
+            "content": ('''
+                당신은 임상 지식을 갖춘 유능하고 신뢰할 수 있는 한국어 기반 의료 어시스턴트입니다.
+                사용자의 질문에 대해 정확하고 신중한 임상 추론을 바탕으로 진단 가능성을 제시해 주세요.
+                반드시 환자의 연령, 증상, 검사 결과, 통증 부위 등 모든 단서를 종합적으로 고려하여 추론 과정과 진단명을 제시해야 합니다.
+                의학적으로 정확한 용어를 사용하되, 필요하다면 일반인이 이해하기 쉬운 용어도 병행해 최대 200 토큰으로 설명해 주세요.
+            '''
+            )
+        },
+        {
+            "role": "user",
+            "content": question.strip()
         }
-    )
+    ]
 
-def _get_llm():
-    global _llm
-    if _llm is None:
-        _llm = _load_model()
-    return _llm
+
 
 # ----------------------------------------------------------
 # External Functions (can be called from outside)
 # ----------------------------------------------------------
 
 async def predict_llm(id: str, llm_memory: redis.Redis) -> ResponseType:
-    _llm = _get_llm()
-
     llm_data = pickle.loads(llm_memory.get(id))
     question = llm_data["inputs"][-1] if llm_data["inputs"] else None
     symptom = llm_data["symptom"][-1] if llm_data["symptom"] else None
 
     if symptom:
-        prompt = _prompts["xray"]
-        input_data = {"symptom": symptom}
+        messages = _build_messages(symptom)
     
     elif question:
-        prompt = _prompts["question"]
-        input_data = {"question": question}
+        messages = _build_messages(question)
     
     else:
         return {"id": id, "llm_result": "Text Data가 없습니다."}
     
-    chain = prompt | _llm
+    prompt_builder = PromptBuilder(tokenizer)
 
-    result = await chain.ainvoke(input_data)
+    result = await llm.ainvoke(prompt_builder.build(messages))
 
-    llm_data["outputs"].append(result)
+    llm_data["outputs"].append(result["content"])
     llm_memory.set(id, pickle.dumps(llm_data))
 
     print(result)
--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app = FastAPI()
 # React 접근 허용
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
